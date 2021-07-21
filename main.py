708d891419dcd26efb990ad9ab2232171967d789
Geoff Sobering <gsobering@verveindustrial.com>
2021-07-21 14:23:41 -0500

    First commit.

---
+++ main.py
@@ -0,0 +1,27 @@
+import types
+
+
+class Thingy():
+    pass
+
+def thingy_method(self) -> str:
+    return f"thingy_method: {self}"
+
+
+if __name__ == '__main__':
+    a = Thingy()
+    a.one = 1
+    a.two = 2
+    a_thingy_method = types.MethodType( thingy_method, a )
+    print(f"a_thingy_method = {a_thingy_method}")
+    print(f"a_thingy_method() = {a_thingy_method()}")
+    a.name = a_thingy_method
+
+    b = Thingy()
+    b.three = 3
+    b.four = 4
+
+    print(f"a.one = {a.one}")
+    print(f"b.four = {b.four}")
+
+    print(f"a.name() = {a.name()}")
